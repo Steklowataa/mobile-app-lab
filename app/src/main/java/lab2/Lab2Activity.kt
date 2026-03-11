2e2a467213f8f14c468b71d5af995e48ba362677
lahodiukoleksandra <81188068+lahodiukoleksandra@users.noreply.github.com>
2026-03-11 16:10:16 +0100

    Lab2 11.03

---
+++ app/src/main/java/lab2/Lab2Activity.kt
@@ -0,0 +1,25 @@
+package lab2
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import pl.wsei.pam.lab01.R
+
+class Lab2Activity : AppCompatActivity() {
+
+    fun onClickBoardSize(v: View) {
+        val tag: String? = v.tag as String?
+        val tokens: List<String>? = tag?.split(" ")
+
+        val rows = tokens?.get(0)?.toIntOrNull() ?: 0
+        val columns = tokens?.get(1)?.toIntOrNull() ?: 0
+
+        Toast.makeText(this, "rows: $rows, columns: $columns", Toast.LENGTH_SHORT).show()
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lab2)
+    }
+}
